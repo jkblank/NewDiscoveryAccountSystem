ee851adf1523c888843c882a04877dab1cb7b596
Blankman <60937724+jkblank@users.noreply.github.com>
2021-09-01 12:35:42 +0200

    Session 2 Continued GeneralResponse added All POM dependencies updated SwaggerConfiguration added RestServiceApplication updated

--- DiscoveryAccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/config/SwaggerConfiguration.java
+++ DiscoveryAccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/config/SwaggerConfiguration.java
@@ -9,29 +9,23 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentaion.spi.DocumentationType;
-import springfox.documentation.spring.web.plugin.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.util.Collections;
-
-@configuration
+@Configuration
 @EnableSwagger2
 @Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfiguration {
-
-    @Value("${swagger.application.version}");
+    @Value("${swagger.application.version}")
     private String applicationVersion;
-
-    @Value("${swagger.application.name}");
+    @Value("${swagger.application.name}")
     private String applicationName;
-
-    @Value("${swagger.application.description}");
+    @Value("${swagger.application.description}")
     private String applicationDescription;
-
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.Swagger_2)
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
@@ -39,16 +33,15 @@ public class SwaggerConfiguration {
                 .pathMapping("/")
                 .apiInfo(apiInfo());
     }
-
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfo(
                 applicationName,
                 applicationDescription,
                 applicationVersion,
-                "",//TermsOfServiceURL
-                new Contact("JRG Klerck",/*URL*/ "", "johnklerck@protonmail.com"),
-                "",//License
-                "",//License URl
+                "",
+                new Contact("John Klerck", "", "johnklerck@protonmail.com"),
+                "",
+                "",
                 Collections.emptyList());
     }
 }
--- DiscoveryAccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/config/WebConfig.java
+++ DiscoveryAccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/config/WebConfig.java
@@ -1,4 +1,12 @@
 package za.ac.nwu.ac.web.sb.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+//find all components in following packages
+@ComponentScan(basePackages = {
+        "za.ac.nwu.ac.web.sb.controller",
+        "za.ac.nwu.ac.web.sb.exception"
+})
 public class WebConfig {
 }
