ab67f3a06971d698ec6ecd14d44a647375706eba
Blankman <60937724+jkblank@users.noreply.github.com>
2021-09-07 02:13:01 +0200

    Other Half of repository setup AccountTransactionRepository

--- DiscoveryAccountSystemRepo/src/main/java/za/ac/nwu/ac/repo/config/RepositoryConfig.java
+++ DiscoveryAccountSystemRepo/src/main/java/za/ac/nwu/ac/repo/config/RepositoryConfig.java
@@ -11,6 +11,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories("za.ac.nwu.ac.repo.persistence")
 @EntityScan("za.ac.nwu.ac.domain.persistence")
-@PropertySource(value="classpath:application-db,properties")
+@PropertySource(value="classpath:application-db.properties")
 public class RepositoryConfig {
 }
--- DiscoveryAccountSystemRepo/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTransactionRepository.java
+++ DiscoveryAccountSystemRepo/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTransactionRepository.java
@@ -2,8 +2,8 @@ package za.ac.nwu.ac.repo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import za.ac.nwu.ac.domain.persistence.AccountType;
+import za.ac.nwu.ac.domain.persistence.AccountTransaction;
 
 @Repository
-public interface AccountTypeRepository extends JpaRepository<AccountType, Long> {
+public interface AccountTransactionRepository extends JpaRepository<AccountTransaction, Long> {
 }
